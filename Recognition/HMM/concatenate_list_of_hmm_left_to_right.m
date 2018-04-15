acd44d1c0f7009b7d47d11191c619a1e016ef066
Gabriel Marzinotto <gabriel.marzinotto@gmail.com>
2018-04-15 20:06:16 +0200

    clean repo

--- Recognition/HMM/concatenate_list_of_hmm_left_to_right.m
+++
@@ -1,8 +0,0 @@
-function [ final_hmm ] = concatenate_list_of_hmm_left_to_right( list_of_hmm )
-
-    final_hmm = list_of_hmm(1);
-    for k = 2:length(list_of_hmm)
-        final_hmm = concatenate_two_hmm_left_to_right( final_hmm , list_of_hmm(k) );
-    end
-
-end
